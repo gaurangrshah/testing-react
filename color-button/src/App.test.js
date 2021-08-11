982cda1747f5902dad69765fe51829261440d1ae
gshah2020 <gaurang.r.shah@gmail.com>
2021-08-11 21:05:46 +0000

    colorButton + disable checkbox

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+test('button has correct initial color and when clicked', () => {
+  render(<App />)
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' })
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+
+  // fireEvent is used to test event based interactions
+  fireEvent.click(colorButton)
+  // after the click the button color should be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' })
+  expect(colorButton.textContent).toBe('Change to red')
+
 });
+
+test('initial button conditions', () => {
+  render(<App/>)
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'})
+  expect(colorButton).toBeEnabled()
+
+  const checkbox = screen.getByRole('checkbox')
+  // using the not method to negate the assertion toBeChecked
+  expect(checkbox).not.toBeChecked()
+})
+
+test('checkbox disables and enables button', () => {
+  render(<App/>)
+  const colorButton = screen.getByRole('button')
+  const checkbox = screen.getByRole('checkbox', {name: 'Disable button'})
+
+  fireEvent.click(checkbox)
+  expect(checkbox).toBeChecked()
+  expect(colorButton).toBeDisabled()
+})
